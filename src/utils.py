e690eb461b41a90c5a4bd16b90121466c4287b44
Ritwick Sundar <ritwick.sundar@gmail.com>
2024-04-05 17:13:39 +0100

    Add skeleton modules for housing core logic and utility functions (#2)

---
+++ src/utils.py
@@ -0,0 +1,30 @@
+"""Module containing utility functions."""
+
+import torch
+import torchvision.models as models
+from PIL import Image
+
+
+def load_model() -> torch.nn.Module:
+    """Loads a pretrained ResNet-18 model.
+
+    Returns:
+        Pretrained ResNet-18 model.
+    """
+    model = models.resnet18(weights="DEFAULT")
+    model.eval()
+
+    return model
+
+
+def load_image(img_path: str) -> Image.Image:
+    """Loads an image from a local path
+
+    Args:
+        img_path: Local path to an image to be read in.
+
+    Returns:
+        Loaded image.
+    """
+    image = Image.open(img_path)
+    return image
